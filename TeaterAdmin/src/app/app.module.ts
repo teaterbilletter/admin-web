6cfd86e9ce97e64d80ea4caf896b39602061ab29
s175130 <32563621+s175130@users.noreply.github.com>
2019-04-08 16:07:03 +0200

    Startet på startside

--- TeaterAdmin/src/app/app.module.ts
+++ TeaterAdmin/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './static_content/footer/footer.component';
+import { NavigationbarComponent } from './static_content/navigationbar/navigationbar.component';
+import { AdminComponent } from './admin/admin.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavigationbarComponent,
+    AdminComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BsDatepickerModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
